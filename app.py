b17717f46f140ce7d5dab1c0794150adf0bdc8f2
KortneeR <kortneer@nycstudents.net>
2024-09-25 10:14:26 -0400

    i did challenge 1 yayayay

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print("shirley")
+""" # Challenge 1 """
+x = 6
+y = 9
+if x % 2 == 0 and y % 2 == 0:
+    print("both are even")
+elif x % 2 == 0 or 2 == 0:
+    print("one is even")
+else:
+    print("both are odd")
