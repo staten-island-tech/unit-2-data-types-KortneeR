5e36de586a7a37ba3eae303f48005f1cd67536e1
KortneeR <kortneer@nycstudents.net>
2024-09-27 10:19:19 -0400

    hihihihihi yayayayayaya

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-""" # Challenge 1 """
-x = 6
+'''# Challenge 1'''
+'''x = 6
 y = 9
 if x % 2 == 0 and y % 2 == 0:
     print("both are even")
 elif x % 2 == 0 or 2 == 0:
     print("one is even")
+elif y % 2 == 0 or 2 == 0:
+    print("one is even")
 else:
-    print("both are odd")
+    print("both are odd")'''
+'''# Challenge 2''' 
+
++990
