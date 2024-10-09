f050240a8c3d0564e4857af8630804d65116ea20
KortneeR <kortneer@nycstudents.net>
2024-10-09 09:45:43 -0400

    ,jkhbv mb

--- app.py
+++ app.py
@@ -21,8 +21,23 @@ elif service == "good":
 else:
     print("25%")'''
  
-# challenge 3
+""" # challenge 3
 a = 45
 for i in range(2, a + 1):
     if a % i == 0:
-        print(i)
+        print(i) """
+
+#challenge 4
+
+# :3 uwu >< meow
+
+# 45 % 2 (1) NOT EQual to 0
+# 45%3(0) equal to 0\\\
+
+# get 2 numbers
+
+#find smaller number
+
+#loop to smaller number +1
+
+# check if i is a factor of BOTH numbers
