5fb866137a04cdbe4be61a223d6b9272135e150d
KortneeR <kortneer@nycstudents.net>
2024-10-15 10:13:12 -0400

    doing madlib thing

--- app.py
+++ app.py
@@ -36,9 +36,20 @@ for i in range(2, a + 1):
 #loop to smaller number +1
 
 # check if i is a factor of BOTH numbers
-
+""" 
 a = 10
 b = 25
 for i in range(1, a + 1):
     if a % i == 0 and b % i == 0:
-        print(i)
+        print(i) """
+
+#Mad Libs Project Part
+#new comment
+"verb1" = "walking"
+"noun1" = "home"
+"celebrity guest" = "Shirley"
+"number" = "983,679,875"
+"noun2" = "meteors"
+"noun3" = "sky"
+"verb2" = "obliterated"
+print("I was walking home with Shirley when 983,679,875 meteors shot out from the sky and obliterated my face.")
